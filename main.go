53206281f94d3205497d6cecd614147cdd096abc
Alexey99 <cache2007@yandex.ru>
2017-08-11 17:01:30 +0300

    Update main.go

--- main.go
+++ main.go
@@ -19,7 +19,13 @@ func main() {
 	// инициализируем канал, куда будут прилетать обновления от API
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
+
 	ch, err := bot.GetUpdatesChan(ucfg) //UpdatesChan(ucfg)
+
+	msg := tgbotapi.NewMessage(402503192, "Hello")
+	mm := SendMsg(msg)
+	bot.Send(mm)
+
 	// читаем обновления из канала
 	for {
 		select {
@@ -33,8 +39,7 @@ func main() {
 			ChatID := update.Message.Chat.ID
 
 			// Текст сообщения
-			Text := update.Message.Text + "  123"
-
+			Text := update.Message.Text + "123"
 
 			log.Printf("[%s] %d %s", UserName, ChatID, Text)
 
@@ -42,6 +47,43 @@ func main() {
 			reply := Text
 			// Создаем сообщение
 			msg := tgbotapi.NewMessage(ChatID, reply)
+
+			//tgbotapi.ReplyKeyboardMarkup.ResizeKeyboard
+			//tgbotapi.KeyboardButton.Text kk := "123"
+			//msg.ReplyMarkup = tgbotapi.ReplyKeyboardMarkup{Keyboard: kk}
+			//tgbotapi.KeyboardButton.
+			//var k1 = tgbotapi.KeyboardButton{Text: "123"}
+			//var k2 = tgbotapi.KeyboardButton{Text: "123"}
+
+			//var test string
+			//test = "TEST"
+
+			var ya string
+			ya = "http://yandex.ru"
+
+			var k1 = []tgbotapi.KeyboardButton{tgbotapi.KeyboardButton{Text: "Contact_1", RequestContact: true}}
+			var k2 = []tgbotapi.KeyboardButton{tgbotapi.KeyboardButton{Text: "Location_2", RequestLocation: true}}
+
+			//var k3 = []tgbotapi.InlineKeyboardButton{tgbotapi.InlineKeyboardButton{Text: "Push", CallbackData: &test}}
+			var k4 = []tgbotapi.InlineKeyboardButton{tgbotapi.InlineKeyboardButton{Text: "Yandex", URL: &ya}}
+
+			kk := [][]tgbotapi.KeyboardButton{}
+
+			kk = append(kk, k1)
+			kk = append(kk, k2)
+
+			kk_ := [][]tgbotapi.InlineKeyboardButton{}
+
+			//kk_ = append(kk_, k3)
+			kk_ = append(kk_, k4)
+
+			var keyboard = tgbotapi.ReplyKeyboardMarkup{Keyboard: kk, ResizeKeyboard: true} //[][]string{[]string{"Hi, Bot!"}}}
+
+			var inkeyboard = tgbotapi.InlineKeyboardMarkup{InlineKeyboard: kk_}
+
+			msg.ReplyMarkup = keyboard
+			msg.ReplyMarkup = inkeyboard
+
 			// и отправляем его
 			mm := SendMsg(msg)
 			bot.Send(mm) //bot.SendMessage(msg)
